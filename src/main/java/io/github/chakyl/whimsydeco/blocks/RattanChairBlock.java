a4047f7ff2cdee848c7d57cec1474dfef7ebaa65
Chakyl <chakyl1127@gmail.com>
2025-09-16 22:54:47 -0400

    Rattan bounding box

--- src/main/java/io/github/chakyl/whimsydeco/blocks/RattanChairBlock.java
+++ src/main/java/io/github/chakyl/whimsydeco/blocks/RattanChairBlock.java
@@ -8,20 +8,23 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class RattanChairBlock extends TallChairBlock {
     public static final VoxelShape UPPER_SHAPE = Shapes.join(
-            box(2, 0, 12, 14, 8, 14),
-            box(4, 7, 12, 12, 8, 14),
+            box(0, 4, 10, 16, 6, 12),
+            box(0.9, -0.1, 9.9, 15.1, 4.1, 12.1),
             BooleanOp.ONLY_FIRST
     );
     public static final VoxelShape LOWER_SHAPE = Shapes.or(
-            box(3, 0, 3, 5, 8, 5),
-            box(11, 0, 3, 13, 8, 5),
-            box(3, 0, 11, 5, 8, 13),
-            box(11, 0, 11, 13, 8, 13),
-            box(2, 8, 2, 14, 10, 14),
-            box(2, 10, 12, 14, 16, 14));
+      box(2, 0, 3, 14, 2, 15),
+      box(7, 2, 13, 9, 16, 15),
+      box(13, 5, 10, 15, 16, 12),
+      box(1, 5, 10, 3, 16, 12),
+      box(1, 5, 0, 3, 7, 10),
+      box(13, 5, 0, 15, 7, 10),
+      box(3, 5, 0, 13, 7, 2),
+      box(3, 4, 2, 13, 6, 13),
+      box(3, 6, 11, 13, 16, 13));
 
     public RattanChairBlock(Properties pProperties) {
-        super(UPPER_SHAPE, LOWER_SHAPE, 12.0D / 16.0D, pProperties);
+        super(UPPER_SHAPE, LOWER_SHAPE, 8.0D / 16.0D, pProperties);
     }
 
 }
